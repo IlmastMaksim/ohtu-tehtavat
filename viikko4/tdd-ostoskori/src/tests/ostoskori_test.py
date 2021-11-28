6049034db0ffa74dadceea54a204f372dc24b810
Maksim Ilmast <maksim.ilmast@yandex.com>
2021-11-28 21:35:35 +0200

    Metodi tyhjenna tyhjentää korin

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -95,3 +95,10 @@ class TestOstoskori(unittest.TestCase):
         ostokset = self.kori.ostokset()
         self.assertEqual(len(ostokset), 0)
     
+    def test_metodi_tyhjenna_tyhjentää_korin(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+
+        self.kori.tyhjenna()
+        ostokset = self.kori.ostokset()
+        self.assertEqual(len(ostokset), 0)
